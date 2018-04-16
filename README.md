9799595b821c49a9a24ee05d967bd4905c04a06c
Dave Martin <growthdesigner@gmail.com>
2018-04-16 16:39:32 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# postmark-firebase
-Postmark + Firebase + Cloud Functions Tutorial
+## Postmark + Firebase + Cloud Functions Tutorial
+
+The following is some demo code that is included within the following blog post (link coming soon!).
+
+### File structure
+
+Here’s a quick structural overview of how this code is organized:
+
+```
+- functions
+  - db
+    - users
+      - onUpdate.f.js
+- public
+  - admin
+    - index.html
+  - google-signin.png
+  - index.html
+```
+
+### Requirements
+
+We won’t cover the Firebase setup process in this tutorial. The [Firebase documentation](https://firebase.google.com/docs/web/setup) is excellent though. If you’re just getting started, you should definitely check it out. From this point forward I’ll assume that you have Firebase set up with [authentication](https://firebase.google.com/docs/auth/web/start), [real-time database](https://firebase.google.com/docs/database/web/start), and [Cloud Functions](https://firebase.google.com/docs/functions/get-started) up and running locally.
---
+++ functions/db/users/onUpdate.f.js
@@ -0,0 +1,42 @@
+
+// 1. Deploys as dbUsersOnUpdate
+const functions = require('firebase-functions')
+const nodemailer = require('nodemailer')
+const postmarkTransport = require('nodemailer-postmark-transport')
+const admin = require('firebase-admin')
+
+// 2. Admin SDK can only be initialized once
+try {admin.initializeApp(functions.config().firebase)} catch(e) {
+        console.log('dbCompaniesOnUpdate initializeApp failure')
+}
+
+// 3. Google Cloud environment variable used:
+// firebase functions:config:set postmark.key="API-KEY-HERE"
+const postmarkKey = functions.config().postmark.key
+const mailTransport = nodemailer.createTransport(postmarkTransport({
+        auth: {
+                apiKey: postmarkKey
+        }
+}))
+
+// 4. Watch for new users
+exports = module.exports = functions.database.ref('/users/{uid}').onCreate((event) => {
+        const snapshot = event.data
+        const user = snapshot.val()
+        // Use nodemailer to send email
+        return sendEmail(user);
+})
+
+function sendEmail(user, company) {
+        // 5. Send welcome email to new users
+        const mailOptions = {
+                from: '"Dave" <dave@example.net>',
+                to: '${user.email}',
+                subject: 'Welcome!',
+                html: `<YOUR-WELCOME-MESSAGE-HERE>`
+        }
+        // 6. Process the sending of this email via nodemailer
+        return mailTransport.sendMail(mailOptions)
+                .then(() => console.log('dbCompaniesOnUpdate:Welcome confirmation email'))
+                .catch((error) => console.error('There was an error while sending the email:', error))
+}
